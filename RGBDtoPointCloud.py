e24999040b7ee8b17acc488d1a2273f6bfb20c08
Sreeharsha Paruchuri <43914425+sreeharshaparuchur1@users.noreply.github.com>
2020-08-23 21:33:34 +0530

    Update RGBDtoPointCloud.py

--- RGBDtoPointCloud.py
+++ RGBDtoPointCloud.py
@@ -11,18 +11,23 @@ def display(image):
     plt.axis("off")
     plt.show()
 
-
 if __name__ == "__main__":
     colour = o3d.io.read_image("./color.jpg")
     depth = o3d.io.read_image("./depth.png")
-    #display(colour)
+    #Reading the images as an image (as opposed to an array).
+    depth = np.asarray(depth)
+    #Now reading the image as a NumPyarray.
+    depthModda = o3d.geometry.Image((depth * 0.001).astype(np.float32))
+    #Scaling the values of depth given and converting to an Image file.
+    o3d.io.write_image("./depthModda.png", depthModda)
+    #Writing the new image file.
+    display(colour)
     display(depth)
-    rgbdImage = o3d.geometry.RGBDImage.create_from_color_and_depth(colour, depth)
-
-    #print(colour.shape)
-    #print(depth.shape)
-    #display(rgbdImage)
-    print(rgbdImage)
+    display(depthModda)
+    rgbdImage = o3d.geometry.RGBDImage.create_from_color_and_depth(colour, depthModda)
+    #Creating the rgbd image.
     pcd = o3d.geometry.PointCloud.create_from_rgbd_image(rgbdImage, o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault))
-    print(pcd)
+    #Giving camera parameters.
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+    #flipping the axis as open3D has +ve y in the direction of gravity
     o3d.visualization.draw_geometries([pcd])
